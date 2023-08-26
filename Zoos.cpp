eaf1e9816a9ea47e77cb8ac958cb44e3f85977ba
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-08-26 21:56:49 +0530

    Zoos.cpp

---
+++ Zoos.cpp
@@ -0,0 +1,24 @@
+include <iostream>
+using namespace std; 
+int main() {
+  string word;
+  int zcount = 0;
+  int ycount = 0;
+  cin>>word;
+  int size = word.length();
+
+  for(int i=0;i<size;i++){
+    if(word[i] == 'z')
+        zcount++;
+    else
+        ycount++;
+  }
+
+    if(ycount == (zcount*2)){
+        cout << "Yes";
+    }
+    else
+        cout << "No";
+
+  return 0;
+}
