5f1f80331865f9f7a4ae1b33048b99d6f3f691aa
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-08-31 23:44:24 +0530

    Create Endless_Appetizers.cpp

---
+++ Endless_Appetizers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T,X,Y,R,plates,sticks;
+    cin>>T;
+    while(T--){
+        cin>>X>>Y>>R;
+        if(R>0){
+            sticks = (R/30)+X;
+        }
+        else sticks = X;
+        
+        if(sticks%Y ==0){
+            plates = (sticks/Y);
+        }
+        else if(sticks%Y !=0)
+        plates =(sticks/Y)+1;
+        cout<<plates<<endl;
+    }
+	return 0;
+}
