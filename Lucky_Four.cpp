9ec1eb7c08d07e87d69351953fbf71fb1aaef5b9
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-09-01 23:21:14 +0530

    Create Lucky_Four.cpp

---
+++ Lucky_Four.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int T;
+	string number;
+    cin>>T;
+    while(T--){
+        int count= 0;
+        cin>>number;
+        int size;
+        size=number.size();
+        for(int i=0;i<size;i++){
+            if(number[i]=='4')
+            count++;
+            
+        }
+        cout<<count<<endl; 
+        
+    }
+	return 0;
+}
