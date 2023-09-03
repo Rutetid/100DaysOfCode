3c7670b546516f8c78994c379ca105f6d0f7e502
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-09-03 23:16:33 +0530

    Create Tour_of_King.cpp

---
+++ Tour_of_King.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,n,r;
+    cin>>t;
+    while(t--){
+        cin>>n>>r;
+        cout<<(n*5)+(r*7)<<endl;
+    }
+    
+	return 0;
+}
