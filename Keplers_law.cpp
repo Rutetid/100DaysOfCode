cbe04cb3b123b7c9dd19e0d0046a5b1045904d7f
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-09-05 01:05:37 +0530

    Create Keplers_law.cpp

---
+++ Keplers_law.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t; 
+    while(t--){
+        float a,b,c,d,A,B,C,D;
+        float k,p;
+        cin>>a>>b>>c>>d;
+        A = a*a;
+        B = b*b;
+        C = c*c*c;
+        D = d*d*d;
+        k = A*D;
+        p = B*C;
+        if(k==p) cout<<"yes"<<endl;
+        else cout<<"no"<<endl;
+        }
+	return 0;
+}
