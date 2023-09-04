92371abcaf596b7eb520f5e10d1853f38f0832bb
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-09-04 23:55:36 +0530

    Create Naive_chef.cpp

---
+++ Naive_chef.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,n,a,b;
+	float proba,probb,ans;
+	cin>>t;
+	while(t--){
+	    cin>>n>>a>>b;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    int counta =0 ;
+	    int countb = 0;
+	    for(int i=0;i<n;i++){
+	         if (a == arr[i])
+	         counta++;
+	    }
+	    for(int i=0;i<n;i++){
+	         if (b == arr[i])
+	         countb++;
+	    }
+	    proba = counta/n;
+	    probb = countb/n;
+	    ans = (proba * probb);
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
