98a370319408660f624d0fafe46e02d6d404423f
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-09-06 01:02:46 +0530

    Create Break_the_Stick.cpp

---
+++ Break_the_Stick.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int c,n,r;
+	cin>>c;
+	while (c--){
+	    cin>>n>>r;
+	    if (n%2 ==0)
+	    cout<<"yes"<<endl;
+	    else if(n%2 == 0 && r%2!=0)
+	    cout<<"yes"<<endl;
+	    else if(n%2 != 0 && r%2!=0)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	}
+	return 0;
+}
