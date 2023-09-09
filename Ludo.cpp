7f9947af3dfeac98a9989b032259ba2d4816efa5
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-09-09 23:42:22 +0530

    Create Ludo.cpp

---
+++ Ludo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,r;
+	cin>>t;
+	while(t--)
+  {
+	    cin>>r;
+	    if(r==6) cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+	}
+	return 0;
+}
