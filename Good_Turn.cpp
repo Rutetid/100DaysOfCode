843d6e05992dfb1de167db64e2186eecd5f28d64
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-09-10 23:08:42 +0530

    Create Good_Turn.cpp

---
+++ Good_Turn.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t,x,y;
+    cin>>t;
+    while(t--){
+        cin>>x>>y;
+        
+        if((x+y)>6) cout<<"yes"<<endl;
+        else cout<<"no"<<endl;
+    }
+	return 0;
+}
