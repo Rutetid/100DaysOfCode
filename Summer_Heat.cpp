fec258e84439ad66b8a2874380304c28743b1d4c
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-09-13 23:54:53 +0530

    Create Summer_Heat.cpp

---
+++ Summer_Heat.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        
+        cout<<(c/a)+(d/b)<<endl;
+    }
+	return 0;
+}
