33c0e3864ce18d6ff99e9bf04c5d8b1dad0bb45f
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-09-18 23:48:53 +0530

    Create Watercons.cpp

---
+++ Watercons.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,water;
+	cin>>t;
+	
+	while(t--){
+	    cin>>water;
+	    if (water>=2000) cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+	}
+	return 0;
+}
