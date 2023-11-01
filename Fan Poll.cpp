167995e5494eab0b924bad66a9e2c0f4000f5b44
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-11-01 23:51:43 +0530

    Create Fan Poll.cpp

---
+++ Fan Poll.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y,z;
+	cin>>x>>y>>z;
+	if(x>y && x>z)
+	cout<<"yes"<<endl;
+	else
+	cout<<"no"<<endl;
+	return 0;
+}
