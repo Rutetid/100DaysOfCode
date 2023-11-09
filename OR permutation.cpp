57dc972b65a1c371cf281aa47dbe44face4e875d
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-11-10 02:17:33 +0530

    Create OR permutation.cpp

---
+++ OR permutation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    for(int i=x;i>=1;i--){
+	        cout<<i<<" ";
+	    }
+	  cout<<endl;
+	}
+	return 0;
+}
