233c5cacbb0f281722e1573fc6f268b267da5f1b
Rutetid <thegamingcasper240@gmail.com>
2023-11-15 22:08:08 +0530

    Created Rivalry.cpp

---
+++ Rivalry.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c,d;
+	cin>>a>>b;
+	cin>>c>>d;
+	if(a+c>b+d){
+	    cout<<"Dominater";
+	    
+	}
+	
+	else cout<<"Everule";
+	return 0;
+}
