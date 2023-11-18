a17b3b42660d760e633027417610b4fcc256debc
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-11-19 01:09:17 +0530

    Create Saving Taxes.cpp

---
+++ Saving Taxes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y;
+    cin>>t;
+    while(t--){
+        cin>>x>>y<<endl;
+        cout<<x-y<<endl;
+    }
+	// your code goes here
+	return 0;
+}
