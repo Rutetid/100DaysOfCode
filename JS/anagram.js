b9e053e2e7cb5d9f7935110ad5fc46bfc0d81e9a
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-11-23 23:53:44 +0530

    Update anagram.js

--- JS/anagram.js
+++ JS/anagram.js
@@ -1,6 +1,6 @@
 /*
   Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
-  What's Anagram?
+  What's Anagram ?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 
   Once you've implemented the logic, test your code by running
