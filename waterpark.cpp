3bcda734efebca654a72000a6d374f9b4a04a4b2
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-11-29 21:31:06 +0530

    created waterpark.cpp

---
+++ waterpark.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	if(a>=60 && b<=130){
+	    cout<<"yes";
+	}
+	
+	else cout<<"no";
+	return 0;
+}
